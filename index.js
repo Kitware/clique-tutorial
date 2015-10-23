b503dd345bb98b4083cf7dc1fac860ab9f4367b2
Roni Choudhury <roni.choudhury@kitware.com>
2015-10-22 22:44:51 -0400

    Base application works

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ window.onload = function () {
     d3.text("facebook-links.txt", function (text) {
         var data,
             nll,
-            nodeTable;
+            nodeTable,
+            adapter,
+            graph,
+            view;
 
-        data = d3.tsv.parseRows(text, function (row, i) {
+        console.log("Loading data...");
+        data = d3.tsv.parseRows(text, function (row) {
             return [Number(row[0]), Number(row[1])];
         });
+        console.log("done");
 
         // Construct a node link list.
         nll = {
@@ -17,6 +22,7 @@ window.onload = function () {
         };
 
         nodeTable = {};
+        console.log("building graph data...");
         _.each(data, function (row) {
             _.each(row, function (node) {
                 if (!_.has(nodeTable, node)) {
@@ -35,7 +41,20 @@ window.onload = function () {
                 data: {}
             });
         });
+        console.log("done");
 
-        console.log(nll);
+        // Create a graph data adapter from the node link data.
+        adapter = new clique.adapter.NodeLinkList(nll);
+
+        // Create a graph model useing the adapter.
+        window.graph = graph = new clique.Graph({
+            adapter: adapter
+        });
+
+        // Create a view to visualize the graph.
+        view = new clique.view.Cola({
+            model: graph,
+            el: "#graph"
+        });
     });
 };
